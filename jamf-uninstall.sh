2074c138c5dcd655a248c10bd636fcce31b77cee
EddieV2 <e_vartanessian@live.com>
2021-09-16 11:48:48 -0400

    Update jamf-uninstall.sh

--- jamf-uninstall.sh
+++ jamf-uninstall.sh
@@ -1,8 +1,5 @@
 # !/bin/bash
-# Matthew Ward
-# Original: 2020.02.10
-# Updated: 2020.09.10
-# v.19
+# v1.0
 
 # This script is intended to disable and remove all Jamf Connect v1 & v2 components.
 # Configuration Profiles will only be removed if they contain com.jamf.connect; If that portion of the script fails, please remove  manually.
